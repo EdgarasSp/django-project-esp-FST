d615ecaa83e4289667da5c0f290a5830441c68d0
Edgaras Spiridonov <dev.applications.esp@gmail.com>
2022-06-07 17:32:54 +0000

    Restored session

--- todo/views.py
+++ todo/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
+from .models import Item
 
 # Create your views here.
 
 
 def get_todo_list(request):
-#    return HttpResponse("Hello!")  esp use this if just want to return some text for testing
-    return render(request, 'todo/todo_list.html')
-
+    items = Item.objects.all()
+    context = {
+        'items': items
+    }
+    return render(request, 'todo/todo_list.html', context)
